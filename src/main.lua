949f2481cdc2d1036fc1e63997db532461313262
MauricioSantos100 <43189404+MauricioSantos100@users.noreply.github.com>
2019-06-22 03:26:21 -0300

    Retirada de arquivos repetidos

--- src/main.lua
+++
@@ -1,9 +0,0 @@
-local tabuleiro = require 'Tabuleiro'
-local pecas = require 'Pecas'
-local jogadores = require 'Jogador'
-
-pedeNomes()
-criaTabuleiro()
-posicionaPecas()
-mostraTabuleiro()
-alternaJogador()
